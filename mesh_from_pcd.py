dcc289a65b8fa6920304c25a28cb9791d5062a08
Param1304 <129577837+Param1304@users.noreply.github.com>
2023-11-07 23:08:19 +0530

    Delete mesh_from_pcd.py

--- mesh_from_pcd.py
+++
@@ -1,5 +0,0 @@
-alpha = 0.03
-print(f"alpha={alpha:.3f}")
-mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
-mesh.compute_vertex_normals()
-o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
