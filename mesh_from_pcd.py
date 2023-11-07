26a79e15a3321be19b7699707710b9489691ee1e
Param1304 <129577837+Param1304@users.noreply.github.com>
2023-11-07 15:49:03 +0530

    Add files via upload

---
+++ mesh_from_pcd.py
@@ -0,0 +1,5 @@
+alpha = 0.03
+print(f"alpha={alpha:.3f}")
+mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
+mesh.compute_vertex_normals()
+o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
