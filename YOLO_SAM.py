012a45b5d4480fa13bb36ac4664e7bed5b4d861f
Param1304 <129577837+Param1304@users.noreply.github.com>
2023-11-07 23:07:20 +0530

    Delete YOLO_SAM.py

--- YOLO_SAM.py
+++
@@ -1,9 +0,0 @@
-import supervision as sv
-import torch
-import os
-import math
-from numpy import random
-import cv2
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-model = models.get('yolo_nas_s', pretrained_weights="coco").to(device)
