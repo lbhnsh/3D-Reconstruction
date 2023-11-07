1277bbc953d918286ec5aad86ad4018e2f7b811b
Param1304 <129577837+Param1304@users.noreply.github.com>
2023-11-07 23:08:29 +0530

    Delete point_cloud.py

--- point_cloud.py
+++
@@ -1,5 +0,0 @@
-color_raw = o3d.io.read_image("./path_of_RGB")
-depth_raw = o3d.io.read_image("./path_of_Depth")
-rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
-    color_raw, depth_raw)
-print(rgbd_image)
