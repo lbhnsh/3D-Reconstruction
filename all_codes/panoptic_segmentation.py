d2c0d776d0d51fd6369bd72d40093aad57889943
lbhnsh <labhanshvnaik2104@gmail.com>
2023-11-08 18:50:26 +0530

    improved file structure

--- all_codes/panoptic_segmentation.py
+++ all_codes/panoptic_segmentation.py
@@ -39,14 +39,6 @@ panoptic_segmentation = processor.post_process_panoptic_segmentation(outputs, ta
 print(panoptic_segmentation.keys())
 
 
-'''
-# COMMENT LATER
-class_names = model.config.id2label
-# Print or display the available classes
-for class_id, class_name in class_names.items():
-    print(f"Class ID: {class_id}, Class Name: {class_name}")
-'''
-
 # Create Segmented Mask
 def draw_panoptic_segmentation(segmentation, segments_info):
     # get the used color map
@@ -80,43 +72,6 @@ def draw_panoptic_segmentation(segmentation, segments_info):
     plt.savefig(panoptic_segmented_path,bbox_inches='tight',pad_inches=0)
     plt.show()
 
-    instance_image = cv2.imread(panoptic_segmented_path) 
-    instance_image = cv2.resize(instance_image,(width,height))
-    instance_image = cv2.cvtColor(instance_image, cv2.COLOR_BGR2RGB)
-    original_image= cv2.imread(path)
-    original_image = cv2.resize(original_image,(width,height))
-    # Reshape the image to a 2D array of pixels
-    pixels = instance_image.reshape((-1, 3)).astype(np.float32)
+draw_panoptic_segmentation(**panoptic_segmentation)
 
-    # Perform k-means clustering
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
-    _, labels, centers = cv2.kmeans(pixels, n_segments, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-
-    # Reshape the labels to match the original image shape
-    labels = labels.reshape(instance_image.shape[:2])
-
-    # Loop through the segments and save each one as a separate image
-    # for segment1_id in range(n_segments):
-    for segment in panoptic_segmentation['segments_info']:
-        segment_id = segment['id']
-        segment_label_id = segment['label_id']
-        segment_label = model.config.id2label[segment_label_id]
-        # Create a mask for the current segment
-        mask = (labels == segment_id).astype(np.uint8)
-        # Apply the mask to the original image
-        segmented_rgb_image = cv2.bitwise_and(original_image, original_image, mask=mask)
-        plt.imshow(segmented_rgb_image)
-        plt.show()
-        # Save the segmented image to a file
-        
-        if segment_label_id in [13, 56, 57, 59, 60, 72, 104, 120, 121]:
-          print(segment_label)
-          output_dir1 = '/home/labhansh/Open3D/segmented_rgb_images'
-          os.makedirs(output_dir1, exist_ok=True)
-          output_file = os.path.join(output_dir1, f'segment_rgb_{segment_label_id}_{segment_id}.png')
-          cv2.imwrite(output_file, segmented_rgb_image)
-
-    return n_segments
-
-n_segments=draw_panoptic_segmentation(**panoptic_segmentation)
-
+# CHANGE THE DIRECTORY PATH AS PER YOUR CHOICE BY CHANGING IT IN LINE 70
